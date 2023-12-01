e4082f8a6ebc2c2e1a230d549cac25c7c15c4d6f
sridattakumar <dattakumar1999@gmail.com>
2023-12-01 10:45:50 +0000

    solution

---
+++ src/components/history/index.js
@@ -0,0 +1,134 @@
+import {Component} from 'react'
+import HistoryItem from '../historyItem'
+
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
+
+// Replace your code here
+class App extends Component {
+  state = {
+    searchInput: '',
+    historyDetailsList: initialHistoryList,
+  }
+
+  deleteItem = id => {
+    const {historyDetailsList} = this.state
+    const filteredUsersData = historyDetailsList.filter(each => each.id !== id)
+    this.setState({
+      historyDetailsList: filteredUsersData,
+    })
+  }
+
+  render() {
+    const {searchInput, historyDetailsList} = this.state
+    const searchResults = historyDetailsList.filter(eachItem =>
+      eachItem.title.includes(searchInput),
+    )
+
+    return (
+      <div className="container">
+        <div className="navbar">
+          <img
+            className="app-logo"
+            alt="app logo"
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+          />
+          <img
+            className="search"
+            src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+            alt="search"
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="Search History"
+            value="searchInput"
+          />
+        </div>
+        <ul className="list-container">
+          {searchResults.map(eachItem => (
+            <HistoryItem
+              HistoryItemDetails={eachItem}
+              key={eachItem.id}
+              deleteHistory={this.deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
+// const HistoryItem = props => {
+//   const {HistoryItemDetails, deleteHistory} = props
+//   const {id, timeAccessed, logoUrl, title, domainUrl} = HistoryItemDetails
+
+//   const onDelete = () => {
+//     deleteHistory(id)
+//   }
+//   return (
+//     <li className="each-item">
+//       <p className="time">{timeAccessed}</p>
+//       <img className="logo" alt="app logo" src={logoUrl} />
+//       <p className="title">{title}</p>
+//       <p className="domain" alt="domain logo">
+//         {domainUrl}
+//       </p>
+//       <img
+//         onClick={onDelete}
+//         className="dlt"
+//         alt="delete"
+//         src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+//       />
+//     </li>
+//   )
+// }
+// export default HistoryItem
